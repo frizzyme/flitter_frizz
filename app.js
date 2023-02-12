0287ae999d1e9ffdfb3267e3afbaf65f377826cb
Melinda <vargamelinda21@gmail.com>
2023-02-12 13:52:33 +0100

    flitter 1stage

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+const middleware = require('./middleware')
+const path = require('path')
+const bodyParser = require("body-parser")
+const mongoose = require("./database");
+const session = require("express-session");
+
+const server = app.listen(port, () => console.log("Server listening on port " + port));
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(session({
+    secret: "bbq chips",
+    resave: true,
+    saveUninitialized: false
+}))
+
+// Routes
+const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
+const postRoute = require('./routes/postRoutes');
+const profileRoute = require('./routes/profileRoutes');
+const uploadRoute = require('./routes/uploadRoutes');
+const searchRoute = require('./routes/searchRoutes');
+
+// Api routes
+const postsApiRoute = require('./routes/api/posts');
+const usersApiRoute = require('./routes/api/users');
+
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+app.use("/posts", middleware.requireLogin, postRoute);
+app.use("/profile", middleware.requireLogin, profileRoute);
+app.use("/uploads", uploadRoute);
+app.use("/search", middleware.requireLogin, searchRoute);
+
+app.use("/api/posts", postsApiRoute);
+app.use("/api/users", usersApiRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+
+    var payload = {
+        pageTitle: "Home",
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user),
+    }
+
+    res.status(200).render("home", payload);
+})
